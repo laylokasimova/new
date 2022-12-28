2b01921ea95564460df0d63c041663bec2644644
laylo <laylokasimovaa@gmail.com>
2022-12-28 15:08:30 +0500

    Global version

---
+++ main.js
@@ -0,0 +1,52 @@
+
+var str = prompt('Введите ваше имя')
+var number = +prompt("Введите ваше возраст");
+
+
+var num = +prompt("15 + 5 ?");
+if (num == 20) {
+  console.log(num);
+  alert("Ваше ответ правильно : 20")
+} else {
+    console.log(num);
+    alert('Ваше ответ не правильно: ' + num + ' Правильный ответ: 20' )
+}
+
+
+
+
+var num = +prompt("15 - 5 ?");
+if (num == 10) {
+  console.log(num);
+  alert("Ваше ответ правильно : 10")
+} else {
+    console.log(num);
+    alert('Ваше ответ не правильно: ' + num + ' Правильный ответ: 10' )
+}
+
+var num = +prompt("5 * 5 ?");
+if (num == 25) {
+  console.log(num);
+  alert("Ваше ответ правильно : 25")
+} else {
+    console.log(num);
+    alert('Ваше ответ не правильно: ' + num + ' Правильный ответ: 25' )
+}
+
+var num = +prompt("5 / 5 ?");
+if (num == 1) {
+  console.log(num);
+  alert("Ваше ответ правильно : 1")
+} else {
+    console.log(num);
+    alert('Ваше ответ не правильно: ' + num + ' Правильный ответ: 1' )
+}
+
+var num = +prompt("5 + 1 = ?");
+if (num == 6) {
+  console.log(num);
+  alert("Ваше ответ правильно : 6")
+} else {
+    console.log(num);
+    alert('Ваше ответ не правильно: ' + num + ' Правильный ответ: 6' )
+}
